0447e1b9c82b667c0bb8a3fdb8966123a06dc9ec
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:11:49 +0900

    MemberUpdateService.java

--- 공부/web_jdbc /member.service /MemberUpdateService.java
+++ 공부/web_jdbc /member.service /MemberUpdateService.java
@@ -4,12 +4,14 @@ import com.webjjang.main.service.Service;
 import com.webjjang.member.dao.MemberDAO;
 import com.webjjang.member.vo.MemberVO;
 
+//회원정보 수정 기능 서비스
 public class MemberUpdateService implements Service {
 
 	@Override
 	public Integer service(Object obj) throws Exception {
-		
+		// DB 처리는 DAO에서 처리 - MemberDAO.update()
+		// MemberController - (Execute) - [MemberUpdateService] - MemberDAO.update()
 		return new MemberDAO().update((MemberVO)obj);
-	}
+	}//end of public Integer
 
-}
+}//end of class
