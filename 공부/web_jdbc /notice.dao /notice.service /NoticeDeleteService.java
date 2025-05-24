33a1f0be59322ca4815f675eb2d90f6a7b45a737
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:18:49 +0900

    NoticeDeleteService.java

--- 공부/web_jdbc /notice.dao /notice.service /NoticeDeleteService.java
+++ 공부/web_jdbc /notice.dao /notice.service /NoticeDeleteService.java
@@ -1,3 +1,7 @@
+/*
+	공지사항 삭제 기능 서비스
+*/
+
 package com.webjjang.notice.service;
 
 import com.webjjang.notice.dao.NoticeDAO;
@@ -8,10 +12,9 @@ public class NoticeDeleteService implements Service {
 
 	@Override
 	public Integer service(Object obj) throws Exception {
-		
 		//DB 처리는 DAO에서 처리 NoticeDAO.delete()
 		//NoticeController -> (Execute로그 출력) -> NoticeDeleteService -> [NoticeDAO.delete()]
 		return new NoticeDAO().delete((Long)obj);
-	}
+	}//end of public Integer
 
-}
+}//end of class
