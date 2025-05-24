7c7eda86260d6bd60b3e8baf624b4d095c95e9f0
leeleeseop <iseobi138@gmail.com>
2025-05-25 00:25:23 +0900

    회원 상세 정보를 조회하는 서비스

--- 공부/web_jdbc /member.service /MemberViewService.java
+++ 공부/web_jdbc /member.service /MemberViewService.java
@@ -15,9 +15,9 @@ public class MemberViewService implements Service {
    
         // MemberDAO의 view 메서드 호출하여 결과를 반환
         return new MemberDAO().view((String)obj);
-		//DB notice에서 리스트 쿼리 실행해서 데이터 가져오기 - 리턴
-		//DB 처리는 DAO에서 처리 NoticeDAO.list()
-		//NoticeController -> (Execute로그 출력) -> NoticeListService -> [NoticeDAO.list()]
+		//DB Member에서 리스트 쿼리 실행해서 데이터 가져오기 - 리턴
+		//DB 처리는 DAO에서 처리 MemberDAO.list()
+		//MemberController -> (Execute로그 출력) -> MemberListService -> [MemberDAO.list()]
 		//if(inc == 1) newdao.increase(no);
 		//return new MemberDAO().view((String)obj);
 	}
