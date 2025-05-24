848a9fd1042d0409cf064ff8c3dbb5718ccf5508
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:09:25 +0900

    MemberLoginService.java

--- 공부/web_jdbc /member.service /MemberLoginService.java
+++ 공부/web_jdbc /member.service /MemberLoginService.java
@@ -4,6 +4,7 @@ import com.webjjang.member.dao.MemberDAO;
 import com.webjjang.member.vo.LoginVO;
 import com.webjjang.main.service.Service;
 
+// 회원 로그인 처리 서비스
 public class MemberLoginService implements Service {
 
 	@Override
@@ -11,5 +12,6 @@ public class MemberLoginService implements Service {
 		// DB 처리는 DAO에서 처리 - MemberDAO.login()
 		// MemberController - (Execute) - [MemberLoginService] - MemberDAO.login()
 		return new MemberDAO().login((LoginVO)obj);
-  }
-}
+  }//end of public Login
+	
+}//end of class
