46d6e785ed07a6c1049807126e88ca6373cc3326
leeleeseop <iseobi138@gmail.com>
2025-05-25 00:33:13 +0900

    MemberWriteService.java

--- 공부/web_jdbc /member.service /MemberWriteService.java
+++ 공부/web_jdbc /member.service /MemberWriteService.java
@@ -11,7 +11,7 @@ public class MemberWriteService implements Service {
 	@Override
 	public Integer service(Object obj) throws Exception {
 	// MemberDAO 객체 생성
-	// MemberDAO의 view 메서드 호출하여 결과를 반환
+	// MemberDAO의 write 메서드 호출하여 결과를 반환
 	return new MemberDAO().write((MemberVO)obj);
 	//DB Member에서 리스트 쿼리 실행해서 데이터 가져오기 - 리턴
 	//DB 처리는 DAO에서 처리 MemberDAO.Write()
