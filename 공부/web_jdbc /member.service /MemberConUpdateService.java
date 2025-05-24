485e4a90e991ec2fcb5786603e5ce763f1a90f63
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:05:06 +0900

    MemberConUpdateService.java

--- 공부/web_jdbc /member.service /MemberConUpdateService.java
+++ 공부/web_jdbc /member.service /MemberConUpdateService.java
@@ -1,7 +1,6 @@
 package com.webjjang.member.service;
 
 import java.util.List;
-
 import com.webjjang.main.service.Service;
 import com.webjjang.member.dao.MemberDAO;
 import com.webjjang.member.vo.MemberVO;
@@ -10,8 +9,8 @@ public class MemberConUpdateService implements Service {
 
 	@Override
 	public Integer service(Object obj) throws Exception {
-
 	// obj는 회원 아이디 String으로 캐스팅
-        return new MemberDAO().updateConDate((String)obj);
-	}
-}
+  return new MemberDAO().updateConDate((String)obj);
+	}//end of public Integer
+	
+}//end of class
