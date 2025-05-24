ff22bee4539c0ac07554d6e6f7293c95b935712c
leeleeseop <iseobi138@gmail.com>
2025-05-25 01:31:11 +0900

    BoardDeleteService.java

--- 공부/web_jdbc /board.service /BoardDeleteService.java
+++ 공부/web_jdbc /board.service /BoardDeleteService.java
@@ -6,17 +6,14 @@ import com.webjjang.main.service.Service;
 
 // 게시판 글 삭제 처리를 담당
 public class BoardDeleteService implements Service {
-
+	
 	@Override
 	public Integer service(Object obj) throws Exception {
 		
 		//넘어오는 데이터의 구조 obj - long[] : obj[0] - no, obj[1] - inc
-		
-		//DB board에서 조회수 1증가 하고 글보기 데이터 가져와서 리턴
-		//1. 조회수 1증가 : inc ==1
-		//DB 처리는 DAO에서 처리 BoardDAO.list()
-		//BoardController -> (Execute로그 출력) -> BoardListService -> [BoardDAO.delet()]
+		//DB 처리는 DAO에서 처리 BoardDAO.delet()
+		//BoardController -> (Execute로그 출력) -> BoardDeleteService -> [BoardDAO.delet()]
 		return new BoardDAO().delete((BoardVO)obj);
-	}
-
-}
+	}//end of public Integer
+	
+}//end of class
