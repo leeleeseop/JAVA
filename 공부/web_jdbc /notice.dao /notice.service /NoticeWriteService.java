759279bda82ead5d64b1bbbe9aa8ddf2e178b4fa
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:23:53 +0900

    NoticeWriteService.java

--- 공부/web_jdbc /notice.dao /notice.service /NoticeWriteService.java
+++ 공부/web_jdbc /notice.dao /notice.service /NoticeWriteService.java
@@ -1,3 +1,7 @@
+/*
+	공지사항 등록 기능 서비스 
+*/
+
 package com.webjjang.notice.service;
 
 import com.webjjang.notice.dao.NoticeDAO;
@@ -5,10 +9,12 @@ import com.webjjang.notice.vo.NoticeVO;
 import com.webjjang.main.service.Service;
 
 public class NoticeWriteService implements Service {
+	
 	@Override
 	public NoticeVO service(Object obj) throws Exception {
 		//DB 처리는 DAO에서 처리 NoticeDAO.write()
 		//NoticeController -> (Execute로그 출력) -> NoticeWriteService -> [NoticeDAO.write()]
 		return new NoticeDAO().write((NoticeVO)obj);
 	}
+	
 }
