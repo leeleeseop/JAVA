d8df8a7983dc795d569bcfc39c67f0dd6fa4cb7c
leeleeseop <iseobi138@gmail.com>
2025-05-25 00:27:09 +0900

    로그인 처리 서비스

--- 공부/web_jdbc /member.service /MemberLoginService.java
+++ 공부/web_jdbc /member.service /MemberLoginService.java
@@ -9,7 +9,7 @@ public class MemberLoginService implements Service {
 	@Override
 	public LoginVO service(Object obj) throws Exception {
 		// DB 처리는 DAO에서 처리 - MemberDAO.view() : obj == no
-		// MemberController - (Execute) - [MemberListService] - MemberDAO.view()
+		// MemberController - (Execute) - [MemberLoginService] - MemberDAO.view()
 		return new MemberDAO().login((LoginVO)obj);
   }
 }
