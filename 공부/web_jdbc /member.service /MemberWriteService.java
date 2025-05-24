db0713eedc399f47767ddea37ac89b0dde25cc46
leeleeseop <iseobi138@gmail.com>
2025-05-25 00:23:50 +0900

    회원 등록(회원가입)의 서비스

---
+++ 공부/web_jdbc /member.service /MemberWriteService.java
@@ -0,0 +1,23 @@
+package com.webjjang.member.service;
+
+import java.util.List;
+
+import com.webjjang.main.service.Service;
+import com.webjjang.member.dao.MemberDAO;
+import com.webjjang.member.vo.MemberVO;
+
+public class MemberWriteService implements Service {
+
+	@Override
+	public Integer service(Object obj) throws Exception {
+    // MemberDAO 객체 생성
+    // MemberDAO의 view 메서드 호출하여 결과를 반환
+    return new MemberDAO().write((MemberVO)obj);
+		//DB Member에서 리스트 쿼리 실행해서 데이터 가져오기 - 리턴
+		//DB 처리는 DAO에서 처리 MemberDAO.list()
+		//MemberController -> (Execute로그 출력) -> MemberListService -> [MemberDAO.list()]
+		//if(inc == 1) newdao.increase(no);
+		//return new MemberDAO().view((String)obj);
+	}
+
+}
