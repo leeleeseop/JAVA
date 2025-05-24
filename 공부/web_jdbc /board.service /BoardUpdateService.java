9a5dee332919b8777f673afb67df51cb89d130e1
leeleeseop <iseobi138@gmail.com>
2025-05-25 01:32:33 +0900

    BoardUpdateService.java

--- 공부/web_jdbc /board.service /BoardUpdateService.java
+++ 공부/web_jdbc /board.service /BoardUpdateService.java
@@ -1,7 +1,5 @@
 package com.webjjang.board.service;
 
-
-
 import com.webjjang.board.dao.BoardDAO;
 import com.webjjang.board.vo.BoardVO;
 import com.webjjang.main.service.Service;
@@ -11,14 +9,10 @@ public class BoardUpdateService implements Service {
 
 	@Override
 	public Integer service(Object obj) throws Exception {
-		
 		//넘어오는 데이터의 구조 obj - long[] : obj[0] - no, obj[1] - inc
-		
-		//DB board에서 조회수 1증가 하고 글보기 데이터 가져와서 리턴
-		//1. 조회수 1증가 : inc ==1
-		//DB 처리는 DAO에서 처리 BoardDAO.list()
-		//BoardController -> (Execute로그 출력) -> BoardListService -> [BoardDAO.list()]
+		//DB 처리는 DAO에서 처리 BoardDAO.update()
+		//BoardController -> (Execute로그 출력) -> BoardUpdateService -> [BoardDAO.update()]
 		return new BoardDAO().update((BoardVO)obj);
-	}
+	}//end of public Integer
 
-}
+}//end of class
