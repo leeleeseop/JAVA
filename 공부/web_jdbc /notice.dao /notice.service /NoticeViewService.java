9b97f2cea554258e7a2413c485794156ef7e75de
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:22:27 +0900

    NoticeViewService.java

--- 공부/web_jdbc /notice.dao /notice.service /NoticeViewService.java
+++ 공부/web_jdbc /notice.dao /notice.service /NoticeViewService.java
@@ -1,3 +1,7 @@
+/*
+	공지사항 상세보기 기능 서비스
+*/
+
 package com.webjjang.notice.service;
 
 import com.webjjang.notice.dao.NoticeDAO;
@@ -5,10 +9,12 @@ import com.webjjang.notice.vo.NoticeVO;
 import com.webjjang.main.service.Service;
 
 public class NoticeViewService implements Service {
+	
 	@Override
 	public NoticeVO service(Object obj) throws Exception {
 		// DB 처리는 DAO에서 처리 - NoticeDAO.view() : obj == no
 		// NoticeController - (Execute) - [NoticeViewService] - NoticeDAO.view()
 		return new NoticeDAO().view((Long)obj);
   }
+	
 }
