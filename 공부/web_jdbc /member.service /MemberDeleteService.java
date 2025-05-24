fa008223142f5ca8160d133f618b431071fdec45
leeleeseop <iseobi138@gmail.com>
2025-05-25 02:06:20 +0900

    MemberDeleteService.java

--- 공부/web_jdbc /member.service /MemberDeleteService.java
+++ 공부/web_jdbc /member.service /MemberDeleteService.java
@@ -4,12 +4,12 @@ import com.webjjang.main.service.Service;
 import com.webjjang.member.dao.MemberDAO;
 import com.webjjang.member.vo.MemberVO;
 
+//회원기능 삭제 서비스
 public class MemberDeleteService implements Service {
 
 	@Override
 	public Integer service(Object obj) throws Exception {
-		
 		return new MemberDAO().delete((MemberVO)obj);
-	}
+	}//end of public Integer
 
-}
+}//end of class
