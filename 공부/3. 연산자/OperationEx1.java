820f95c7bfce22934e87c2d4494b6cb55e7deb69
leeleeseop <iseobi138@gmail.com>
2025-06-10 13:20:53 +0900

    나누기 연산자

---
+++ 공부/3. 연산자/OperationEx1.java
@@ -0,0 +1,17 @@
+
+public class OperationEx1 {
+
+	public static void main(String[] args) {
+		
+		int num  = 13;
+		//int / int -> int : 소수점 이하는 잘린다.
+		System.out.println(num + " / 3 = " + (num / 3));
+		
+		// 나머지 
+		System.out.println(num + " % 3 = " + (num % 3));
+		
+		// 나누기 연산은 0으로 할 수 없다.
+		// System.out.println(num/0);
+	}
+
+}
